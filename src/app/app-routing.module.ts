ac215fd5b4f4395c6d3f0cbfecb0f11a8cc1eaa5
JpAmorim <118912933+JpAmori@users.noreply.github.com>
2022-12-22 11:31:08 -0300

    Melhoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login"
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/loginModel.ts
@@ -0,0 +1,4 @@
+export class loginModel {
+    username!: string;
+    password!: string;
+}
