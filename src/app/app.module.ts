26800fcf59a39da19704b57e8f41e16c60f0ffa3
JpAmori <joao.pedro.am.profissional@gmail.com>
2022-12-27 14:29:44 -0300

    Melhorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from 'src/app/home.component/home.component';
+import { HomeComponent } from 'src/app/home/home.component';
 import { LoginComponent } from './login/login.component'
 
 @NgModule({
