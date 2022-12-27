4eae593b2c9105610a9ae2553efd57fb031f3d96
JpAmori <joao.pedro.am.profissional@gmail.com>
2022-12-27 11:14:49 -0300

    Melhorias

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(){ }
 
+  criateCont(){
+    const loginContainer = document.getElementById('loginContainer');
+
+    const moveOverlay = () => loginContainer!.classList.toggle('move');
+
+    document.getElementById('open-register')!.addEventListener('click', moveOverlay)
+    document.getElementById('open-login')!.addEventListener('click', moveOverlay)
+
+    document.getElementById('open-register-mobile')!.addEventListener('click', moveOverlay)
+    document.getElementById('open-login-mobile')!.addEventListener('click', moveOverlay)
+                               
+  }
 }
+
+
