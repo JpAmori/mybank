3ac9476a8f6c261c9a0b2a3213c714b771927cf9
JpAmori <joao.pedro.am.profissional@gmail.com>
2022-12-27 13:30:28 -0300

    Melhorias

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,8 +13,8 @@ export class LoginComponent {
 
     const moveOverlay = () => loginContainer!.classList.toggle('move');
 
-    document.getElementById('open-register')!.addEventListener('click', moveOverlay)
-    document.getElementById('open-login')!.addEventListener('click', moveOverlay)
+    document.getElementById('open-register')!.addEventListener("click", moveOverlay)
+    document.getElementById('open-login')!.addEventListener("click", moveOverlay)
 
     document.getElementById('open-register-mobile')!.addEventListener('click', moveOverlay)
     document.getElementById('open-login-mobile')!.addEventListener('click', moveOverlay)
