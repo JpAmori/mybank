221bb1158042f19ff886c7fe39ca9989bfd95549
JpAmori <joao.pedro.am.profissional@gmail.com>
2022-12-27 14:29:29 -0300

    Melhorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    pathMatch: "full",
-    redirectTo: "login"
-  },
-  
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'ERRO', }
 ];
 
 @NgModule({
